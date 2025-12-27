67e3859de8baeedd443120f955ef734658fcb766
michalwegiel <michalwegiel95@gmail.com>
2025-12-27 23:58:22 +0100

    Add docstring in world module

--- src/core/entities/world.py
+++ src/core/entities/world.py
@@ -3,6 +3,19 @@ from dataclasses import dataclass
 
 @dataclass
 class World:
+    """
+    Represents the current state of the game world.
+
+    Attributes
+    ----------
+    location: str
+        Name of the current location or area in the game world.
+    quest: str
+        Currently active quest.
+    weather: str
+        Current weather conditions affecting the world, default: 'clear'.
+    """
+
     location: str
     quest: str
     weather: str = "clear"
